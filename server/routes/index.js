99f2fe7766172cce5f701a8f9948f348eea66514
Xinghao Cheng <chengxinghao93@gmail.com>
2018-04-17 09:52:30 -0400

    session and logout

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,16 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+function isAuthenticated(req, res, next) {
+	if(req.isAuthenticated()){
+		next();
+	}else{
+		res.redirect("/login");
+	}
+}
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 'Express' });
 });
 
 router.get('/login', function(req, res, next) {
-  res.render('index', { title: 'Login' });
+	res.render('index', { title: 'Login' });
 });
 
-router.get('/good', function(req, res, next) {
-  res.render('index', { title: req.user.name });
+router.get('/good', isAuthenticated, function(req, res, next) {
+	res.render('main', { title: req.user.name });
+});
+
+router.get('/logout', isAuthenticated, function(req, res, next) {
+	req.session.destroy(function (err) {
+		res.redirect('/');
+	});
 });
 
 module.exports = router;
