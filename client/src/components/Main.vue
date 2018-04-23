24ee2f84c8afb69a5d7125575cf4cb578896fb44
Yifeng Liu <yifeng.liu@duke.edu>
2018-04-23 16:10:43 -0400

    user-interface

--- client/src/components/Main.vue
+++ client/src/components/Main.vue
@@ -2,17 +2,49 @@
 <template>
   <div >
     <MainNav/>
+    <form class="form-inline d-flex justify-content-center" v-on:submit.prevent="search">
+      <input type="text" class="form-control col-4" placeholder="Search Jobs">
+      <input type="text" class="form-control col-4 ml-2" placeholder="Search Location">
+      <button type="submit" class="btn btn-outline-light ml-2">Search</button>
+    </form>
+    <ul>
+      <li v-for="job in jobs" class="mr-3 mt-3">
+        <Job :job="job" />
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import MainNav from "./MainNav.vue"
+import MainNav from "./MainNav.vue";
+import Job from "./Job.vue";
+import data from "../JSON/data.json"
+
 export default {
   name: 'Main',
+  data(){
+    return{
+      jobs: data
+    }
+  },
   components:{
-    MainNav
+    MainNav,
+    Job
+  },
+  methods:{
+    search: function(){
+      
+    }
   }
 }
 </script>
 <style scoped>
+  form{
+    padding:10px;
+    background-color: #5c6f7c;
+  }
+  li{
+    list-style: none;
+    display: inline-block;
+  }
 </style>
